82223f65ac5063073b8d0ccdd9d24c9946f4bcca
Sara Pettigrew <sarapettigrew@MacBook-Pro.local>
2020-10-07 20:52:44 -0600

    Lucky number found

--- src/demonstration_3.py
+++ src/demonstration_3.py
@@ -19,5 +19,58 @@ Input: arr = [1,1,2,2,2]
 Output: -1
 """
 def find_lucky(lst):
-    # Your code here
-
+    # UNDERSTAND
+    # if the count = value, return the value
+    # PLAN
+    # Define an output variable
+    # overall runtime:
+    # constant + n log n (for the sort) + n * n --> O(n^2)
+    # 1 + n log n + n^2
+    # space complexity: O(n)
+    output = -1             # constant
+    # Sort the list from largest to smallest
+    lst.sort(reverse=True)  # sort --> O(n log n), in place
+    # Make a set
+    set_list = set(lst)     # O(n)
+    # Iterate through the set, counting the # of times each value shows up
+    for number in set_list:             # how many times does the loop run? potentially n times.
+        # Make a count variable
+        count = lst.count(number)       # O(n): the number of operations grows with the size of lst
+        # Check if the count == the value
+        if number == count:             # O(1)
+            # If it does, add it to output variable
+            output = number             # O(1)
+    # If nothing matches, return -1
+    return output
+# def count(lst, number):
+#     count = 0
+#     for n in lst:
+#         if n == number:
+#             count += 1
+#     return count
+#
+def find_lucky_2(lst):
+    # overall runtime with count outside of loop:
+    # 1 + n log n  + n --> n log n
+    # space complexity: O(n)
+    output = -1             # constant
+    # Sort the list from largest to smallest
+    lst.sort(reverse=True)  # sort --> O(n log n), in place
+    # count the number of times each element appears in the lst
+    counts = {}
+    for number in lst:      # O(n)
+        if number not in counts:
+            counts[number] = 0
+        counts[number] += 1
+    # Make a set
+    set_list = set(lst)     # O(n)
+    # Iterate through the set, counting the # of times each value shows up
+    for number in set_list:             # how many times does the loop run? potentially n times.
+        # Make a count variable
+        count = counts[number]           # O(1)
+        # Check if the count == the value
+        if number == count:             # O(1)
+            # If it does, add it to output variable
+            output = number             # O(1)
+    # If nothing matches, return -1
+    return output
